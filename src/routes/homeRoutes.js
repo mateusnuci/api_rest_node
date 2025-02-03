3851035941e714f14e059666ba847db16843e90e
Mateus Nuci <mateusnucicosta@gmail.com>
2025-02-03 19:32:04 -0300

    feat: implement routes

---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import HomeController from '../controllers/HomeController';
+
+const router = new Router();
+
+router.get('/', HomeController.index);
+
+
+
+
+export default router;
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import UserController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
+
+const router = new Router();
+
+//router.get('/', UserController.index);
+//router.get('/:id', UserController.show)
+
+
+router.post('/', UserController.store);
+router.put('/', loginRequired, UserController.update)
+router.delete('/', loginRequired, UserController.delete)
+
+
+
+
+export default router;

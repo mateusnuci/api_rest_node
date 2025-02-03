b3b93c51f77c7d58ff792cfbe3e6fb6c97c2f4cb
Mateus Nuci <mateusnucicosta@gmail.com>
2025-02-03 19:28:48 -0300

    feat: add main files

---
+++ app.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import './src/database'; // importando o arquivo de connection
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+import userRoutes from './src/routes/userRoutes';
+import tokenRoutes from './src/routes/tokenRoutes';
+
+
+class App { // classe p configuração do express
+  constructor() {
+    this.app = express(); // express
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  routes() {
+    this.app.use("/", homeRoutes);
+    this.app.use("/users", userRoutes);
+    this.app.use("/tokens", tokenRoutes);
+  }
+
+
+}
+
+export default new App().app; // exportando o express

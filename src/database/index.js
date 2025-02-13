24d09b9570c9b9951b77feda8b7c392f943fb8be
Mateus Nuci <mateusnucicosta@gmail.com>
2025-02-13 09:39:00 -0300

    feat: add create photo table migration and uploads

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 import Sequelize from 'sequelize';
 import Student from '../models/Student';
 import User from '../models/User';
+import Photo from '../models/Photo'
 import databaseConfig from '../config/database';
 
-const models = [Student, User];
+const models = [Student, User, Photo];
 const connection = new Sequelize(databaseConfig);
 
 models.forEach(model => model.init(connection));
+models.forEach(model => model.associate && model.associate(connection.models)); // short circuit
